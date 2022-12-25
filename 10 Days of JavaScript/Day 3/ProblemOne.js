449b0d7b30591094f3fd5adda901a7238df6b487
jzhupan <jzhupan@gmail.com>
2022-12-24 23:53:44 -0500

    Work in progress.

---
+++ 10 Days of JavaScript/Day 3/ProblemOne.js
@@ -0,0 +1,11 @@
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+  // Complete the function
+  nums.sort(function (a, b) {
+    return a - b;
+  });
+}
