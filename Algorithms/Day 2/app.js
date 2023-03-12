93d49c107a844a6c5fd6330113c3b7897e3b9d45
jzhupan <jzhupan@gmail.com>
2023-03-11 20:00:30 -0500

    Solved problem 3

---
+++ Algorithms/Day 2/app.js
@@ -0,0 +1,12 @@
+// 3. A very Big Sum
+function aVeryBigSum(ar) {
+    // Write your code here
+  let bigSum = 0
+  for (let i =0; i < ar.length; i++){
+    bigSum += ar[i]
+  }
+  console.log(bigSum)
+  return bigSum
+}
+
+//4. Diagonal Difference
