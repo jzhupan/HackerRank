d830525758ce2b3bcd08bdc6af5fe0e5cb2a6c29
jzhupan <jzhupan@gmail.com>
2023-07-19 19:32:33 -0400

    Was able to solve Number Line Jumps problems

--- Algorithms/Day 11 - Number Line Jumps/app.js
+++ Algorithms/Day 11 - Number Line Jumps/app.js
@@ -48,3 +48,85 @@ Explanation 1
 The second kangaroo has a starting location that is ahead (further to the right) of the first kangaroo's starting location (i.e., x₂ > x₁ ). Because the second kangaroo moves at a faster rate (meaning v₂ > v₁) and is already ahead of the first kangaroo, the first kangaroo will never be able to catch up. Thus, we print NO.
 
 */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    let kangarooOneJumps = 0
+    let kangarooTwoJumps = 0
+    let kangarooOnePosition = x1
+    let kangarooTwoPosition = x2
+    let kangarooOneSteps = v1
+    let kangarooTwoSteps = v2
+    
+    if(kangarooTwoPosition > kangarooOnePosition && kangarooTwoSteps > kangarooOneSteps){
+        return "NO"
+    } else if(kangarooOnePosition > kangarooTwoPosition && kangarooOneSteps > kangarooTwoSteps){
+        return "NO"
+    } else {
+        for(let step = 0; step < 10001; step++){
+            kangarooOnePosition += kangarooOneSteps
+            kangarooTwoPosition += kangarooTwoSteps
+            kangarooOneJumps++
+            kangarooTwoJumps++
+            if(kangarooOnePosition == kangarooTwoPosition && kangarooOneJumps == kangarooTwoJumps){
+                return "YES"
+            }
+        }
+        return "NO"
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const x1 = parseInt(firstMultipleInput[0], 10);
+
+    const v1 = parseInt(firstMultipleInput[1], 10);
+
+    const x2 = parseInt(firstMultipleInput[2], 10);
+
+    const v2 = parseInt(firstMultipleInput[3], 10);
+
+    const result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
