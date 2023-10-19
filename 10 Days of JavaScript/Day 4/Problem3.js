d8bcffaead5ba6b74401dd61182498791dfc8015
jzhupan <jzhupan@gmail.com>
2023-10-19 13:59:37 -0400

    HackerRank daily challenge

--- 10 Days of JavaScript/Day 4/Problem3.js
+++ 10 Days of JavaScript/Day 4/Problem3.js
@@ -1,2 +1,11 @@
 //Classes
 
+class Polygon {
+    constructor(measurements){
+      this.getPerimeters = measurements.reduce((a, b) => a + b, 0);
+    }
+    
+    perimeter(){
+      return this.getPerimeters;
+    }
+  }
