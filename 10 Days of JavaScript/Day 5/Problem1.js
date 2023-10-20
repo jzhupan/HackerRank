474cbce4938b3f415a473deae115a48e9e442553
jzhupan <jzhupan@gmail.com>
2023-10-20 15:52:40 -0400

    Daily HackerRank Challenge

--- 10 Days of JavaScript/Day 5/Problem1.js
+++ 10 Days of JavaScript/Day 5/Problem1.js
@@ -1,1 +1,18 @@
 //Inheritance
+
+class Rectangle {
+    constructor(w, h){
+        this.w = w;
+        this.h = h;
+    }
+}
+
+Rectangle.prototype.area = function()
+{    return this.w * this.h;}
+
+
+class Square extends Rectangle {    
+    constructor(side){        
+        super(side,side)    
+    }
+}
