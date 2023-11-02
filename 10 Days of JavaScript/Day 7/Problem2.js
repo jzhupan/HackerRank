647d1950ed08e03d67362c0a92dff024d20a4bbf
jzhupan <jzhupan@gmail.com>
2023-11-02 11:09:05 -0400

    HackerRank Challenge

--- 10 Days of JavaScript/Day 7/Problem2.js
+++ 10 Days of JavaScript/Day 7/Problem2.js
@@ -1,2 +1,16 @@
 //Regular Expressions II
 
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    let re = /^(Mr\.|Dr\.|Er\.|Ms\.|Mrs\.)[A-Z][a-z]*$/
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
